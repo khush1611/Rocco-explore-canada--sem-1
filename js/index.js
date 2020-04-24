0b7391f9a0d660a67077196814d876a263ac98a5
khush1611 <khushboo.umrigar16@gmail.com>
2020-04-24 19:48:15 -0400

    primary color has been changed. hamburger menu has been added

--- js/index.js
+++ js/index.js
@@ -1,13 +1,34 @@
-// storing the element into variable
-let $searchBar = window.document.querySelector(".header-searchbar");
+//``````````  SEARCH BAR ``````````// 
+    // storing the element into variable for searchbar
+    let $searchBar = window.document.querySelector(".header-searchbar");
 
-function stickyNavigation(){
-    if(window.scrollY > 100){
-        $searchBar.classList.add("header-searchbar-sticked");
+    //creating a fucntion to make the searchbar stickey
+    function stickeyNavigation(){
+        if(window.scrollY > 100){
+            $searchBar.classList.add("header-searchbar-sticked");
+        }
+        else {
+            $searchBar.classList.remove("header-searchbar-sticked");
+        }
     }
-    else {
-        $searchBar.classList.remove("header-searchbar-sticked");
-    }
-}
 
-window.addEventListener('scroll', stickyNavigation)
+    // calling the fucntion stickeynavigation
+    window.addEventListener('scroll', stickeyNavigation)
+
+
+
+//``````````  HAMBURGER ``````````// 
+
+    // storing element into variable
+    let $hamburger = window.document.querySelector('#hamburger');
+    let $sidebar =  window.document.querySelector('#sidebar');
+    let $sidebarAllElements = window.document.querySelector('.header-sidebar__col');
+
+
+    //creating a fucntion to make the searchbar stickey
+    function hamburgerClick(){
+        $hamburger.classList.toggle('hamburger__icon-click')
+         $sidebar.classList.toggle('onClick-sidebar');
+         $sidebarAllElements.classList.toggle('visibilty-hidden');
+    }
+    $hamburger.addEventListener('click', hamburgerClick)
